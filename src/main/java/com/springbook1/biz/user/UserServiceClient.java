9f975c26560d67b54cfb316c5a490f55b93287bd
ChoiSHy <tjdgus4697@gmail.com>
2023-02-15 17:48:28 +0900

    접속 id, 비밀번호 변경

--- src/main/java/com/springbook1/biz/user/UserServiceClient.java
+++ src/main/java/com/springbook1/biz/user/UserServiceClient.java
@@ -14,8 +14,8 @@ public class UserServiceClient {
 		
 		// 3. 로그인 기능 테스트
 		UserVO vo = new UserVO();
-		vo.setId("user1");
-		vo.setPassword("user1");
+		vo.setId("test");
+		vo.setPassword("test");
 		
 		UserVO user = userService.getUser(vo);
 		if(user!=null)
